ae057c9b55d199e1dc461176b0fe1b618802cc29
JulissaDietzeNarvaez <julissa1904@hotmail.com>
2021-11-18 18:13:36 +0100

    18/11/2021 18:13

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+$(".open-menu-btn").on("click", function () {
+    if ($("body").hasClass("closed-menu")) {
+      $("body").removeClass("closed-menu");
+    } else $("body").addClass("closed-menu");
+  });  
+
