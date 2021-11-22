14a45e9452feb173b922a1028ed5392bb829eb58
JulissaDietzeNarvaez <julissa1904@hotmail.com>
2021-11-22 17:24:59 +0100

    22-11-2021 16:15

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,22 @@
 $(".open-menu-btn").on("click", function () {
-    if ($("body").hasClass("closed-menu")) {
-      $("body").removeClass("closed-menu");
-    } else $("body").addClass("closed-menu");
-  });  
+  if ($("body").hasClass("closed-menu")) {
+    $("body").removeClass("closed-menu");
+  } else $("body").addClass("closed-menu");
+}); 
 
-var coll = document.getElementsByClassName("collapsible");
+
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var submenu = this.nextElementSibling;
-    if (submenu.style.display === "block") {
-      submenu.style.display = "none";
-    } else {
-      submenu.style.display = "block";
-    }
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+  dropdownContent.style.display = "none";
+  } else {
+  dropdownContent.style.display = "block";
+  }
   });
 }
