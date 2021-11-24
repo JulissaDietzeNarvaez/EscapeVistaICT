679995cd9935b8d0189380230ae411562c7e0ad7
JulissaDietzeNarvaez <julissa1904@hotmail.com>
2021-11-24 11:50:49 +0100

    24/11/2021 11:50

--- JS/script.js
+++ JS/script.js
@@ -5,18 +5,3 @@ $(".open-menu-btn").on("click", function () {
 }); 
 
 
-
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
-
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-  this.classList.toggle("active");
-  var dropdownContent = this.nextElementSibling;
-  if (dropdownContent.style.display === "block") {
-  dropdownContent.style.display = "none";
-  } else {
-  dropdownContent.style.display = "block";
-  }
-  });
-}
