4518efcc22471af8090e5039b2b65579ae1daafa
JulissaDietzeNarvaez <julissa1904@hotmail.com>
2021-11-25 15:58:45 +0100

    25/11/2021 16:15

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,22 @@
+/* Side menu */
 $(".open-menu-btn").on("click", function () {
   if ($("body").hasClass("closed-menu")) {
     $("body").removeClass("closed-menu");
   } else $("body").addClass("closed-menu");
 }); 
 
+/* Accordion voor opleidingen */
+var acc = document.getElementsByClassName("accordion");
+var i;
 
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
